619643a4ea54add2c7e54cae64425dac7dd7a9d3
Bachir <bachir@Bachirs-MacBook-Pro.local>
2022-07-14 00:40:18 -0400

    add css style to the header and meme components

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function Header() {
     return(
-        <div className="head">
-            hello
+        <div className="header">
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--text">automatic</h4>
         </div>
     )
 }
